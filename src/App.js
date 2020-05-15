178e57d7c3ab6892b16a7d7c932f8ed3116d333d
Juha Isopoussu <isopoju@gmail.com>
2020-05-15 12:54:07 +0300

    Added environmental settings ver 0.1, npm i react-scripts dotenv dotenv-expand

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React, { useState, useEffect } from 'react'
 import pingService from './services/ping'
+console.log(require('dotenv').config())
 
 function App() {
   const [reply, setReply] = useState([])
 
+  console.log('process.env.NODE_ENV', process.env.NODE_ENV)
+  console.log(
+    'process.env.REACT_APP_HYSQL_SRV',
+    process.env.REACT_APP_HYSQL_SRV
+  )
+  console.log('process.env', process.env)
+
+  const hysqlSrv = process.env.REACT_APP_HYSQL_SRV
+
   useEffect(() => {
-    pingService
-      .ping()
-      .then(initReply => {
-        console.log('initReply', initReply)
-        setReply(initReply)
-      })
-  }, [])
+    pingService.ping(hysqlSrv).then((initReply) => {
+      console.log('initReply', initReply)
+      setReply(initReply)
+    })
+  }, [hysqlSrv])
 
   return (
     <div>
--- src/services/ping.js
+++ src/services/ping.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 const baseUrl = '/api/ping'
 
-const ping = async () => {
-  const response = await axios.get(`${baseUrl}`)
+const ping = async (hysqlSrv) => {
+  const response = await axios.get(`${hysqlSrv}${baseUrl}`)
   return response.data
 }
 
