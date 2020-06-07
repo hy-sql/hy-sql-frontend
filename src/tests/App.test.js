a1c85058fc0d19f0647abfb7c63b9321c32972b9
Markus Kononen <43392343+Cell9@users.noreply.github.com>
2020-06-07 04:00:18 +0300

    changed for tests localhost -> netlify

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,14 +4,14 @@ import App from '../App'
 
 describe('Page renders', () => {
   it('successfully loads', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit('https://hy-sql.netlify.app')
     cy.get('h1').contains('HY-SQL').should('be.visible')
   })
 })
 
 describe('<App />', () => {
   it('ping and its reply are rendered', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit('https://hy-sql.netlify.app')
     cy.wait(500)
 
     cy.get('div').contains('ping').should('be.visible')
@@ -21,7 +21,7 @@ describe('<App />', () => {
 
 describe('<App />', () => {
   it('ping is received', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit('https://hy-sql.netlify.app')
     cy.wait(500)
     cy.request('GET', 'https://hy-sql.herokuapp.com/api/ping').as('pong')
     cy.get('@pong').should((response) => {
